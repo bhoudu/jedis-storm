9c5506816f26bef2951c2d4ef6d04aa6055953a3
Benjamin Houdu <benjamin.houdu@gmail.com>
2015-02-09 11:07:24 +0100

    Removed dependency to apache commons lang 3.

--- src/main/java/org/zenbeni/jedis/lua/LuaHelper.java
+++ src/main/java/org/zenbeni/jedis/lua/LuaHelper.java
@@ -7,7 +7,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 import java.util.Scanner;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
